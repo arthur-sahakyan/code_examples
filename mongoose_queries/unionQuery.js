3d8404d6dff801144dd943c1354b99998c1a0014
“Artur” <artursahakyan262@gmail.com>
2022-10-02 23:33:26 +0400

    changed unionwith

--- mongoose_queries/unionQuery.js
+++ mongoose_queries/unionQuery.js
@@ -1,23 +1,26 @@
 // this type query used when you want get data from multiple collections with one query
 
 function unionQuery() {
-    const filter = {roleType: 'user'}
-    // you can for each query give own filter by $match, and get own fields with $project
+    const filter = { roleType: 'user' }
+        // you can for each query give own filter by $match, and get own fields with $project
 
-    return UserModel.aggregate([
-        {
+    return UserModel.aggregate([{
             $match: filter
         },
         {
             $project: {
-                _id: 1, firstName: 1, lastName: 1, email: 1, consumerId: 1, avatar_path: 1, markets: 1,
-                paidForSubscription: 1, freeTrial: 1, 'status': 'Active'
+                _id: 1,
+                firstName: 1,
+                lastName: 1,
+                email: 1,
+                avatar_path: 1,
+                'status': 'Deleted'
             }
         },
         {
             $unionWith: {
-                coll: 'DeletedUsers', pipeline: [
-                    {
+                coll: 'DeletedUsers',
+                pipeline: [{
                         $match: filter
                     },
                     {
@@ -26,7 +29,6 @@ function unionQuery() {
                             firstName: 1,
                             lastName: 1,
                             email: 1,
-                            consumerId: 1,
                             avatar_path: 1,
                             'status': 'Deleted'
                         }
